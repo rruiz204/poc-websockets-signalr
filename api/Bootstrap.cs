a2818bfcf2d429b0931e97066d74f5ce4cd4bc48
roney ruiz <roneyruizrojas@gmail.com>
2024-11-27 21:21:23 -0600

    feat: configure api services

---
+++ api/Bootstrap.cs
@@ -0,0 +1,22 @@
+namespace Api;
+
+public static class Bootstrap
+{
+  public static void AddVitalServices(this IServiceCollection services)
+  {
+    AddCors(services);
+    services.AddSwaggerGen();
+    services.AddControllers();
+    services.AddEndpointsApiExplorer();
+  }
+
+  public static void AddCors(IServiceCollection services)
+  {
+    services.AddCors(options => 
+    {
+      options.AddPolicy(name: "Local", policy => {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+      });
+    });
+  }
+}
